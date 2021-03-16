c8ca3359a4bac51740b27ed7262bb5f2c668178d
Gabriel <knuckles4x@gmail.com>
2021-03-16 20:28:10 -0300

    new databse url

--- ormconfig.js
+++ ormconfig.js
@@ -1,11 +1,7 @@
 require("dotenv").config();
 module.exports = {
   type: "postgres",
-  host: process.env.DB_HOST,
-  port: process.env.PORT,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  url: process.env.DATABASE_URL,
   synchronize: true,
   logging: false,
   migrations: ["src/database/migrations/*.ts"],
