2cc79a88d8755fec9028b7465b938d2e78ecb040
Gabriel <knuckles4x@gmail.com>
2021-03-16 21:34:11 -0300

    change build dir and orm configs

--- ormconfig.js
+++ ormconfig.js
@@ -4,8 +4,8 @@ module.exports = {
   url: process.env.DATABASE_URL,
   synchronize: true,
   logging: false,
-  migrations: ["src/database/migrations/*.ts"],
-  entities: ["src/app/models/*.ts"],
+  migrations: ["dist/database/migrations/*.js"],
+  entities: ["dist/app/models/*.js"],
   cli: {
     entitiesDir: "src/entity",
     migrationsDir: "src/database/migrations",
